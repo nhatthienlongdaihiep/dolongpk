77212dac9ea8d4aa2e7d7672858886f7ff4e1d57
Tan Nguyen <tannt@appota.com>
2016-10-06 14:06:11 +0700

    by Tan

--- application/modules/servers/views/FRONTEND/listServer.php
+++ application/modules/servers/views/FRONTEND/listServer.php
@@ -4,5 +4,8 @@
 	<li><a target='_blank' href="<?=PATH_URL.'choi-game/'.$vl->slug?>" ><i></i><span><?=CutText($vl->name,20)?></span><p></p></a></li> 
 	<?php if($k == 5) break; } ?>
 </ul>
+<div class='btn_maychu_more'>
+	<a href="<?=PATH_URL?>may-chu">Xem thêm >> </a>
+</div>
 <?php }?>
 
